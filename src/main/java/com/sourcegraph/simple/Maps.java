9282b0cb14560b542023286461ec06c1ecb15b18
Beyang Liu <beyang@sourcegraph.com>
2017-02-03 15:27:57 -0800

    inner class

---
+++ src/main/java/com/sourcegraph/simple/Maps.java
@@ -0,0 +1,10 @@
+package com.sourcegraph.simple;
+
+/**
+ * Created by beyang on 2/3/17.
+ */
+public class Maps {
+
+    public static class FooMap {
+    }
+}
---
+++ src/main/java/com/sourcegraph/simple/SpecialMap.java
@@ -0,0 +1,10 @@
+package com.sourcegraph.simple;
+
+import com.sourcegraph.simple.Maps.FooMap;
+
+/**
+ * Created by beyang on 2/3/17.
+ */
+public class SpecialMap extends FooMap {
+
+}
