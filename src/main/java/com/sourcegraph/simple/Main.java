3eeb2af3cec3d1a2f1269c858e00106755c03837
Beyang Liu <beyang@sourcegraph.com>
2017-02-05 18:38:01 -0800

    add class named "Map"

--- src/main/java/com/sourcegraph/simple/Main.java
+++ src/main/java/com/sourcegraph/simple/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String... args) throws Exception {
         System.out.println("Hello world");
     }
+
+    public void useMap() {
+        Map map = new Map();
+    }
 }
---
+++ src/main/java/com/sourcegraph/simple/Map.java
@@ -0,0 +1,7 @@
+package com.sourcegraph.simple;
+
+/**
+ * Created by beyang on 2/5/17.
+ */
+public class Map {
+}
