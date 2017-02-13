c128626964fc82d93c9e16bfb34b9f2ada73537d
Beyang Liu <beyang@sourcegraph.com>
2017-02-13 15:00:58 -0800

    Iterables.java

---
+++ src/main/java/com/sourcegraph/simple/Iterables.java
@@ -0,0 +1,10 @@
+package com.sourcegraph.simple;
+
+/**
+ * Created by beyang on 2/13/17.
+ */
+public final class Iterables {
+    public static <F, T> Iterable<T> transform() {
+        return null;
+    }
+}
